dae105b916051f81979a6b6a667c0bec951e26ee
ProgrammerDuck2 <43863123+ProgrammerDuck2@users.noreply.github.com>
2024-01-26 14:56:22 +0100

    moved packages, added non-vr movement

---
+++ Assets/Scripts/S_PcCamera_TB.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class S_PcCamera_TB : MonoBehaviour
+{
+    GameObject POV;
+
+    float xRotation;
+
+    int lookUpAngle = -15;
+    int lookDownAngle = 90;
+
+    private void Start()
+    {
+        POV = transform.parent.gameObject;
+    }
+    void Update()
+    {
+        float MouseX = Input.GetAxis("Mouse X") * (S_Settings_TB.MouseSensitivity * Time.deltaTime) / Time.timeScale;
+        float MouseY = Input.GetAxis("Mouse Y") * (S_Settings_TB.MouseSensitivity * Time.deltaTime) / Time.timeScale;
+
+        if (!float.IsNaN(Vector3.up.y * MouseX))
+        {
+            POV.transform.Rotate(Vector3.up * MouseX);
+        }
+        if (!float.IsNaN(MouseY))
+        {
+            xRotation -= MouseY;
+            xRotation = Mathf.Clamp(xRotation, lookUpAngle, lookDownAngle);
+            POV.transform.localRotation = Quaternion.Euler(xRotation, POV.transform.eulerAngles.y, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/S_Settings_TB.cs
@@ -0,0 +1,4 @@
+public static class S_Settings_TB
+{
+    public static int MouseSensitivity = 500;
+}
