86674c52dc0a2aefa5054edf6462e8cf5b9e2808
ProgrammerDuck2 <43863123+ProgrammerDuck2@users.noreply.github.com>
2024-01-31 16:06:51 +0100

    more grab things

--- Assets/Scripts/Player/S_Grab_TB.cs
+++ Assets/Scripts/Player/S_Grab_TB.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class S_Grab_TB : MonoBehaviour
 {
+    [Header("Player")]
+    GameObject playerBody;
+
+    [Header("Controller")]
+    XRController controller;
+
     [Header("Contolls")]
     [SerializeField] InputActionProperty trigger;
     bool triggerActivated = false;
     [SerializeField] InputActionProperty grip;
     bool gripActivated = false;
+
+    bool grab = false;
 
     void Start()
     {
@@ -18,18 +27,35 @@ public class S_Grab_TB : MonoBehaviour
         trigger.action.canceled += toggleTrigger;
         grip.action.started += toggleGrip;
         grip.action.canceled += toggleGrip;
+
+        playerBody = transform.parent.parent.parent.gameObject;
+        controller = GetComponent<XRController>();
     }
 
     private void Update()
     {
-        if(triggerActivated && gripActivated)
+        if ((triggerActivated && gripActivated) != grab)
+        {
+            initializedGrab();
+        }
+
+        grab = triggerActivated && gripActivated ? true : false;
+
+        if (grab)
         {
             Grab();
         }
     }
+    void initializedGrab()
+    {
+        S_Movement_TB movePlayer = playerBody.GetComponent<S_Movement_TB>();
+        movePlayer.enabled = !movePlayer.enabled;
+
+        //controller.enabled = !controller.enabled;
+    }
     void Grab()
     {
-        print("omg such grab");
+        
     }
 
     void toggleTrigger(InputAction.CallbackContext context)
