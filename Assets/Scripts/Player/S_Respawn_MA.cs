7f8aeeaa7ce12b2b3920a972a7aff99dee005f44
ingermarie040 <ingermarie04@gmail.com>
2024-02-01 17:22:31 +0100

    respawn on all ground objects

--- Assets/Scripts/Player/S_Respawn_MA.cs
+++ Assets/Scripts/Player/S_Respawn_MA.cs
@@ -6,6 +6,35 @@ public class S_Respawn_MA : MonoBehaviour
 {
     [SerializeField] private GameObject respawnPoint;
     [SerializeField] private float outOfWorld;
+    S_Movement_TB movement;
+    private bool hasHappened;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = GetComponent<S_Movement_TB>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (movement.Grounded == true)
+        {
+            if(hasHappened == false)
+            {
+                Debug.Log("new ground");
+                respawnPoint.transform.position = gameObject.transform.position;
+                hasHappened = true;
+            }
+        }
+        else
+        {
+            hasHappened = false;
+        }
+
+    }
+
+    //movement.Grounded - gets the grounded
 
     void FixedUpdate()
     {
