fb1e03f2b58920ac7e895aeadb05877d6be37983
ingermarie040 <ingermarie04@gmail.com>
2024-02-03 22:05:44 +0100

    halth bar

---
+++ Assets/S_HealthBar_MA.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class S_HealthBar_MA : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        lider.maxValue = health;
+        slider.value = health;
+    }
+    
+    public void SetHealth(int health)
+    {
+        slider.value = health; 
+    }
+}
