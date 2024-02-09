87b0a4e5203fbd63d075c228c319caf27c16b805
ingermarie040 <ingermarie04@gmail.com>
2024-02-09 02:25:50 +0100

    idk how i didnt break down tbh (playerhealth)

--- Assets/Scripts/Player/S_PlayerHealth_MA.cs
+++ Assets/Scripts/Player/S_PlayerHealth_MA.cs
@@ -7,12 +7,23 @@ public class S_PlayerHealth_MA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        S_Stats_MA.playerHealth = S_Stats_MA.maxHealth;
+        //Debug.Log(S_Stats_MA.playerHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (S_Stats_MA.playerHealth <= 0)
+        {
+            transform.position = GetComponent<S_Respawn_MA>().respawnPoint;
+            //Debug.Log(GetComponent<S_Respawn_MA>().respawnPoint);
+            Debug.Log(transform.position);
+            S_Stats_MA.playerHealth = S_Stats_MA.maxHealth;
+        }
+    }
+    private void LateUpdate()
+    {
+        Debug.Log(transform.position);
     }
 }
