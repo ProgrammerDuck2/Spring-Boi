d60c24f916cd5a15d95ce7b26fa2e8765fd06f4d
Takotzo <oyvindroberg@hotmail.no>
2024-02-23 14:23:02 +0100

    Particle For Punching

--- Assets/!Scripts/Player/Hand/S_PunchParticle_OR.cs
+++ Assets/!Scripts/Player/Hand/S_PunchParticle_OR.cs
@@ -9,13 +9,11 @@ public class S_PunchParticle_OR : MonoBehaviour
 {
     [SerializeField] private ParticleSystem impactParticle;
     [SerializeField] private ParticleSystem[] hitParticle;
-    private void OnCollisionEnter(Collision other)
-    {
-        ParticlesOnImpact();
-    }
-
+    
+    // Access Method from script when want to be used
     public void ParticlesOnImpact()
     {
+        // Select Random text to display
         var random = Random.Range(0, hitParticle.Length);
         var particle = Instantiate(impactParticle, transform.position, Quaternion.identity);
         particle.Play();
