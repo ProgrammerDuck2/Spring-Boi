d243544624d703b81dc2e2a08144bcb214c526f4
ingermarie040 <ingermarie04@gmail.com>
2024-02-27 18:25:58 +0100

    call weworks??

--- Assets/!Scripts/TabletUI/S_CallTablet_MA.cs
+++ Assets/!Scripts/TabletUI/S_CallTablet_MA.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class S_CallTablet_MA : MonoBehaviour
 {
+    //call should always be page 0;
+
     private GameObject player;
     PlayerInput PlayerInput;
+
+    [SerializeField] private TMP_Text callText;
+    [SerializeField] private List<string> callList = new List<string>();
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +25,21 @@ public class S_CallTablet_MA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GetComponent<S_Ipad_MA>().isActive)
-        {
 
-        }
     }
 
     void Interact(InputAction.CallbackContext context)
     {
+        StartCoroutine(CallSpeech());
+    }
 
+    IEnumerator CallSpeech()
+    {
+        for (int i = 0; i < callList.Count; i++)
+        {
+            callText.text = callList[i];
+            yield return new WaitForSeconds(3);
+            print(callList[i]);
+        }
     }
 }
--- Assets/!Scripts/TabletUI/S_TabletPages_MA.cs
+++ Assets/!Scripts/TabletUI/S_TabletPages_MA.cs
@@ -1,8 +1,4 @@
-using NaughtyAttributes;
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
-using System.Xml.Schema;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,57 +7,70 @@ public class S_TabletPages_MA : MonoBehaviour
     private GameObject player;
     PlayerInput PlayerInput;
 
-    [SerializeField] private List<GameObject> tabletPages = new List<GameObject>();
-    private int currentPage = 0;
-    private float XX;
+    public List<GameObject> tabletPages = new List<GameObject>();
+
+    private int currentPage = 1;
+    private float leftOrRight;
     private float waitTime = 0;
+    private int firstPage = 1;
+
+    [SerializeField] private GameObject triggerCall;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindFirstObjectByType<S_Movement_TB>().gameObject;
         PlayerInput = player.GetComponent<PlayerInput>();
-        XX = PlayerInput.actions["TabletPages"].ReadValue<Vector2>().x;
+        leftOrRight = PlayerInput.actions["TabletPages"].ReadValue<Vector2>().x;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(currentPage);
+        if (triggerCall.GetComponent<S_CallTrigger_MA>().tabletTrigger)
+        {
+            firstPage = 0;
+        }
+
         waitTime += Time.deltaTime;
-        if (GetComponent<S_Ipad_MA>().isActive) 
+        if (GetComponent<S_Ipad_MA>().isActive)
         {
-        XX = PlayerInput.actions["TabletPages"].ReadValue<Vector2>().x;
-        if (waitTime >= 0.2f)
-        {
-            if (XX > 0)
+
+            leftOrRight = PlayerInput.actions["TabletPages"].ReadValue<Vector2>().x;
+            if (waitTime >= 0.2f)
             {
-                if (currentPage == 0)
+                if (leftOrRight > 0) //counts up
                 {
-                    currentPage = tabletPages.Count - 1;
+                    if (currentPage == firstPage)
+                    {
+                        currentPage = tabletPages.Count - 1;
+                    }
+                    else
+                    {
+                        currentPage--;
+                    }
                 }
-                else
+                if (leftOrRight < 0) //counts down
                 {
-                    currentPage--;
+                    if (currentPage >= tabletPages.Count - 1)
+                    {
+                        currentPage = firstPage;
+                    }
+                    else
+                    {
+                        currentPage++;
+                    }
                 }
+                waitTime = 0;
             }
-            if (XX < 0)
+            for (int i = 0; i < tabletPages.Count; i++)
             {
-                if (currentPage >= tabletPages.Count - 1)
-                {
-                    currentPage = 0;
-                }
-                else
-                {
-                    currentPage++;
-                }
+                if(i != currentPage)
+                    tabletPages[i].gameObject.SetActive(false);
             }
-            waitTime = 0;
+            tabletPages[currentPage].gameObject.SetActive(true);
         }
-        for (int i = 0; i < tabletPages.Count; i++)
-        {
-            tabletPages[i].gameObject.SetActive(false);
-        }
-        tabletPages[currentPage].gameObject.SetActive(true);
-        }
+        //Debug.Log(triggerCall);
     }
 }
