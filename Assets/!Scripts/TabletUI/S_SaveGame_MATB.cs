1c403a9634a3949c9c657aaab1c1f9c20500cab2
ingermarie04 <ingermarie04@gmail.com>
2024-03-01 14:54:50 +0100

    torje sucks

--- Assets/!Scripts/TabletUI/S_SaveGame_MATB.cs
+++ Assets/!Scripts/TabletUI/S_SaveGame_MATB.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class S_SaveGame_MATB : MonoBehaviour //, S_ButtonInterface
+public class S_SaveGame_MATB : MonoBehaviour, S_ButtonInterface_TBMA
 {
     // Start is called before the first frame update
     void Start()
