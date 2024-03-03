f173ff4c89816a6216b7fabe21879f00e587279f
ingermarie040 <ingermarie04@gmail.com>
2024-03-03 17:20:50 +0100

    heppp

--- Assets/!Scripts/TabletUI/S_CallTrigger_MA.cs
+++ Assets/!Scripts/TabletUI/S_CallTrigger_MA.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class S_CallTrigger_MA : MonoBehaviour
 {
     [HideInInspector] public bool tabletTrigger = false;
-    [SerializeField] private AudioClip ringtone;
+    [SerializeField] AudioClip ringtone;
+    [SerializeField] List<string> speech = new List<string>();
 
     GameObject Ipad;
+    GameObject callTablet;
 
     // Start is called before the first frame update
     void Start()
     {
         Ipad = FindFirstObjectByType<S_Ipad_MA>().gameObject;
+        callTablet = FindFirstObjectByType<S_CallTablet_MA>().gameObject;
     }
 
     // Update is called once per frame
@@ -28,6 +31,7 @@ public class S_CallTrigger_MA : MonoBehaviour
             tabletTrigger = true;
             Debug.Log(tabletTrigger);
             PlayRingtone();
+            callTablet.GetComponent<S_CallTablet_MA>().callList = speech;
         }
     }
     void PlayRingtone()
