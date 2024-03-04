5a2d90006464daa548a7ec6d3c040d1412ff3780
ProgrammerDuck2 <43863123+ProgrammerDuck2@users.noreply.github.com>
2024-03-04 11:26:10 +0100

    playing with interfaces

--- Assets/!Scripts/TabletUI/TabletManagers/S_Interactable_TBMA.cs
+++ Assets/!Scripts/TabletUI/TabletManagers/S_Interactable_TBMA.cs
@@ -7,7 +7,7 @@ public interface S_Interactable_TBMA
     public Color ButtonColor { get; set; }
     public Color HighlightColor { get; set; }
     public Color PressedColor { get; set; }
-    public void OnClickEnter();
+    public virtual void OnClickEnter() { Debug.Log("hi"); }
     public void OnClickExit();
 
     public void OnHover();
