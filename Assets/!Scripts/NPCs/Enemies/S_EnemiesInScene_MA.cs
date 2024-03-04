5b95dd29e3f7779bc1b1d3f714047f8a591d5604
ingermarie04 <ingermarie04@gmail.com>
2024-03-04 10:50:16 +0100

    enemies all ready for playtesters

--- Assets/!Scripts/NPCs/Enemies/S_EnemiesInScene_MA.cs
+++ Assets/!Scripts/NPCs/Enemies/S_EnemiesInScene_MA.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class S_EnemiesInScene_MA : MonoBehaviour
 {
     [SerializeField] private GameObject enemiesDeadText;
     private GameObject enemiesDead;
     private bool text = true;
+    [SerializeField] GameObject button;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +18,24 @@ public class S_EnemiesInScene_MA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(transform.childCount == 0)
+        //if(transform.childCount == 0)
+        //{
+        //    if (text == true)
+        //    {
+        //        enemiesDead = Instantiate(enemiesDeadText);
+        //        text = false;
+        //    }
+        //}
+        //if (text == false)
+        //{
+        //    if (Input.GetKeyDown(KeyCode.E))
+        //    {
+        //        enemiesDead.gameObject.SetActive(false);
+        //    }
+        //}
+        if (transform.childCount == 0)
         {
-            if (text == true)
-            {
-                enemiesDead = Instantiate(enemiesDeadText);
-                text = false;
-            }
-        }
-        if (text == false)
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                enemiesDead.gameObject.SetActive(false);
-            }
+            button.GetComponent<Image>().color = button.GetComponent<Button>().colors.pressedColor;
         }
     }
 }
