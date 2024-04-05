8213dcb2f05f02adadbfc68fc204c75ba80ade77
ProgrammerDuck2 <torjemyhre@gmail.com>
2024-04-05 15:41:51 +0200

    fixes

--- Assets/S_InteractableObject_TB.cs
+++ Assets/S_InteractableObject_TB.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class S_InteractableObject_TB : MonoBehaviour
 {
 
     public virtual void Interact(S_Hand_TB hand)
     {
-
+        hand.hapticFeedback.TriggerHaptic(.3f, .1f, hand.GetComponent<ActionBasedController>());
     }
 
     public virtual void EndInteract(S_Hand_TB hand)
