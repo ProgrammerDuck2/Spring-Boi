0502e3321828916769adcce3a3b0ff666bd6bd2d
ingermarie04 <ingermarie04@gmail.com>
2024-05-10 15:27:12 +0200

    gearhaven changes

--- Assets/!!Scripts/Objects/Platforms/S_MovingPlatform_MA.cs
+++ Assets/!!Scripts/Objects/Platforms/S_MovingPlatform_MA.cs
@@ -9,16 +9,22 @@ public class S_MovingPlatform_MA : MonoBehaviour
     [Range(0, 1)]
     [SerializeField] float value;
 
+    [SerializeField] float startPos;
+
     Transform Location1;
     Transform Location2;
 
     [SerializeField] bool reverse;
+    bool hasRun;
 
     private void Start()
     {
         lever = FindFirstObjectByType<S_Lever_TB>();
         Location1 = transform.parent.GetChild(1); //gets start
-        Location2 = transform.parent.GetChild(2); //end
+        Location2 = transform.parent.GetChild(2); //gets end
+
+        startPos = transform.localPosition.y;
+
     }
 
     private void Update()
@@ -26,9 +32,18 @@ public class S_MovingPlatform_MA : MonoBehaviour
         if (lever == null) return;
         if (lever.active == false) return;
 
-        transform.localPosition = Vector3.Lerp(Location1.localPosition, Location2.localPosition, value); //slideren
+       //slider, starts at objects position first time
+        if (!hasRun)
+        {
+            transform.localPosition = Vector3.Lerp(Location1.localPosition, Location2.localPosition, startPos);
+            hasRun = true;
+        }
+        if (hasRun)
+        {
+            transform.localPosition = Vector3.Lerp(Location1.localPosition, Location2.localPosition, value);
+        }
 
-
+        //reverse
         value += reverse ? -Time.deltaTime * speed : Time.deltaTime * speed;
         if (value > 1) reverse = true;
         if (value < 0) reverse = false;
