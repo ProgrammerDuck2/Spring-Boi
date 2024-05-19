8c2220a36d771fc2a6bc474e23726574e0c48588
ingermarie04 <ingermarie04@gmail.com>
2024-05-19 23:28:19 +0200

    asdfghj

--- Assets/!!Scripts/TabletUI/Status/S_OilSlider_MA.cs
+++ Assets/!!Scripts/TabletUI/Status/S_OilSlider_MA.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,10 +13,18 @@ public class S_OilSlider_MA : MonoBehaviour
         S_Stats_MA.oilCollected = 0;
         slider.maxValue = S_Stats_MA.maxOil;
         slider.value = S_Stats_MA.oilCollected;
+
+        print(S_Stats_MA.maxOil);
     }
 
     private void Update()
     {
         slider.value = S_Stats_MA.oilCollected;
+    }
+
+    [Button]
+    public void addOild()
+    {
+        S_Stats_MA.oilCollected++;
     }
 }
