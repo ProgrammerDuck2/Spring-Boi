7a543130b6b25d2541545bfbc32a689b20332a1a
BossmanCEO <hkalstad@gmail.com>
2024-05-29 00:30:52 +0200

    added voiceacting

--- Assets/!!Scripts/Audio/S_FMODEvents_HA.cs
+++ Assets/!!Scripts/Audio/S_FMODEvents_HA.cs
@@ -30,6 +30,38 @@ public class S_FMODEvents_HA : MonoBehaviour
 
     [field: SerializeField] public EventReference punch { get; private set; }
 
+
+    [field: Header("SpringDadDialogueSpringtown")]
+
+    [field: SerializeField] public EventReference SpringdadDialogueSpringtown { get; private set; }
+
+
+    [field: Header("SpringetteDialogueSpringtown")]
+
+    [field: SerializeField] public EventReference SpringetteDialogueSpringtown { get; private set; }
+
+
+    [field: Header("SpringDadDialoguePistonrow")]
+
+    [field: SerializeField] public EventReference SpringdadDialoguePistonrow { get; private set; }
+
+
+    [field: Header("SpringDadDialogueMagneticfields")]
+
+    [field: SerializeField] public EventReference SpringdadDialogueMagneticfields { get; private set; }
+
+
+    [field: Header("SpringDadDialogueBatteryavenue")]
+
+    [field: SerializeField] public EventReference SpringdadDialogueBatteryavenue { get; private set; }
+
+
+    [field: Header("SpringDadDialogueGearhaven")]
+
+    [field: SerializeField] public EventReference SpringdadDialogueGearhaven { get; private set; }
+
+
+
     public static S_FMODEvents_HA instance { get; private set; }
 
     private void Awake()
