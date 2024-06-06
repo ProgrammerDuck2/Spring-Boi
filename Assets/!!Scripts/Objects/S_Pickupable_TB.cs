53f1cdd4c39b941c0106362df0e2391c03cd5aa5
BossmanCEO <hkalstad@gmail.com>
2024-06-06 15:05:00 +0200

    skidibi

--- Assets/!!Scripts/Objects/S_Pickupable_TB.cs
+++ Assets/!!Scripts/Objects/S_Pickupable_TB.cs
@@ -10,7 +10,7 @@ public class S_Pickupable_TB : S_InteractableObject_TB
     Collider col;
 
     NavMeshAgent agent;
-    S_EnemyFight_TB enemy;
+    S_EnemyFight enemy;
 
     private void Start()
     {
@@ -20,7 +20,7 @@ public class S_Pickupable_TB : S_InteractableObject_TB
         if(isEnemy)
         {
             agent = GetComponent<NavMeshAgent>();
-            enemy = GetComponent<S_EnemyFight_TB>();
+            enemy = GetComponent<S_EnemyFight>();
         }
     }
 
